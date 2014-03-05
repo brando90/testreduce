0a0ea5dfad05a5cbdaa4ad051366de00f4f6ab8e
Brando Miranda <brando90@mit.edu>
2014-03-05 15:46:41 -0500

    perf stats initial file

---
+++ performance_stats.js
@@ -0,0 +1,8 @@
+var conn = new Connection({'host': host, 'port': port, 'keyspace': 'Keyspace1', 'cql_version': '2.0.0'});
+var pool = new PooledConnection({'hosts': hosts, 'keyspace': 'Keyspace1', 'cql_version': '2.0.0'});
+
+
+
+
+
+
