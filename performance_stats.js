2a501075250517e26a3ef902ca6878913949a0db
Brando Miranda <brando90@mit.edu>
2014-03-05 16:58:08 -0500

    commiting

--- performance_stats.js
+++ performance_stats.js
@@ -1,7 +1,25 @@
-var conn = new Connection({'host': host, 'port': port, 'keyspace': 'Keyspace1', 'cql_version': '2.0.0'});
-var pool = new PooledConnection({'hosts': hosts, 'keyspace': 'Keyspace1', 'cql_version': '2.0.0'});
+//var conn = new Connection({'host': host, 'port': port, 'keyspace': 'Keyspace1', 'cql_version': '2.0.0'});
+//var pool = new PooledConnection({'hosts': hosts, 'keyspace': 'Keyspace1', 'cql_version': '2.0.0'});
 
+var Connection = require('cassandra-client').Connection;
+var con = new Connection({host:'cassandra-host', port:9160, keyspace: "testreducedb");
+con.connect(function(err, con) {
+  if (err) {
+    // Failed to establish connection.
+		console.log("Failed to establish connection BRANDO!");
+    throw err;
+  }
 
+/*
+	con.execute('UPDATE Standard1 SET ?=? WHERE key=?', ['cola', 'valuea', 'key0'], function(err) {
+      if (err) {
+          // handle error
+      } else {
+          // handle success.
+      }
+  });
+*/
+});
 
 
 
