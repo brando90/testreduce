3db3cee802f670120cd9bed41b6d45d9f0b6d863
Brando Miranda <brandomiranda@ubuntu.(none)>
2014-04-05 23:32:44 -0400

    helper function that I wrote to insert elements to a sorted javascript array.

---
+++ insert_function.js
@@ -0,0 +1,35 @@
+function insert(array, element){
+  if(array.length == 0){
+    return [element];
+  }
+  var index = insertRec(array, element, 0, array.length - 1);
+  array.splice(index, 0, element)
+  return array
+}
+
+function insertRec(array, element, i, j){
+  if(j <= i){
+    if(element <= array[i]){
+      return i;
+    }else{
+      return i+1;
+    }
+  }
+  var index = Math.floor((j - i + 1)/2);
+  var middle = array[index];
+  if( element <= middle ){
+    return insertRec(array, element, i, index - 1);
+  }else{
+    return insertRec(array, element, index + 1, j);
+  }
+}
+
+
+// console.log( insert([], -1), " answer = ", [-1] );
+// console.log( insert([0], -1), " answer = ", [-1, 0] );
+// console.log( insert([0], 1), " answer = ", [0, 1] );
+// console.log( insert([0, 1, 3], 2), "answer = ", [0, 1, 2, 3]);
+// console.log( insert([0, 1, 1.5, 2, 3], 1.5), "answer = ", [0, 1, 1.5, 2, 3]);
+
+exports.insert = insert;
+exports.insertRec = insertRec;
