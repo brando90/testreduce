dfb687749c05a70c90f915c7a2df4e8956d0aa0f
Brando Miranda <brandomiranda@ubuntu.(none)>
2014-04-06 19:19:48 -0400

    debugged the jUnit test for my insert function. It should be working and it doesn't seem its going to an infinite recursion.

--- insert_function.js
+++ insert_function.js
@@ -3,7 +3,54 @@ function insert(array, element){
     return [element];
   }
   var index = getIndexPositionToInsert(array, element);
+  return insertIntoPosition(array, element, index);function insert(array, element){
+  if(array.length == 0){
+    return [element];
+  }
+  var index = getIndexPositionToInsert(array, element);
   return insertIntoPosition(array, element, index);
+}
+
+function insertIntoPosition(array, element, index){
+  array.splice(index, 0, element)
+  return array
+}
+
+/**
+* returns the index where to insert the element to the sorted array.
+**/
+function getIndexPositionToInsert(array, element){
+  return positionToInsertRec(array, element, 0, array.length - 1);
+}
+
+function positionToInsertRec(array, element, i, j){
+  if(j <= i){
+    if(element <= array[i]){
+      return i;
+    }else{
+      return i+1;
+    }
+  }
+  var index = Math.floor((j - i + 1)/2);
+  var middle = array[index];
+  if( element <= middle ){
+    return positionToInsertRec(array, element, i, index - 1);
+  }else{
+    return positionToInsertRec(array, element, index + 1, j);
+  }
+}
+
+
+// console.log( insert([], -1), " answer = ", [-1] );
+// console.log( insert([0], -1), " answer = ", [-1, 0] );
+// console.log( insert([0], 1), " answer = ", [0, 1] );
+// console.log( insert([0, 1, 3], 2), "answer = ", [0, 1, 2, 3]);
+// console.log( insert([0, 1, 1.5, 2, 3], 1.5), "answer = ", [0, 1, 1.5, 1.5, 2, 3]);
+
+exports.insert = insert;
+exports.insertIntoPosition = insertIntoPosition;
+exports.getIndexPositionToInsert = getIndexPositionToInsert;
+exports.positionToInsertRec = positionToInsertRec;
 }
 
 function insertIntoPosition(array, element, index){
--- jUnit_file_for_insert_function.js
+++ jUnit_file_for_insert_function.js
@@ -1,7 +1,81 @@
 var ins = require("./insert_function.js");
 
-console.log( ins.insert([], -1), " answer = ", [-1] );
-console.log( ins.insert([0], -1), " answer = ", [-1, 0] );
-console.log( ins.insert([0], 1), " answer = ", [0, 1] );
-console.log( ins.insert([0, 1, 3], 2), "answer = ", [0, 1, 2, 3]);
-console.log( ins.insert([0, 1, 1.5, 2, 3], 1.5), "answer = ", [0, 1, 1.5, 1.5, 2, 3]);
+// attach the .compare method to Array's prototype to call it on any array
+//from stackoverflow. Can't believe this is not a native function in javascript
+//http://stackoverflow.com/questions/7837456/comparing-two-arrays-in-javascript
+Array.prototype.compare = function (array) {
+    // if the other array is a falsy value, return
+    if (!array)
+        return false;
+
+    // compare lengths - can save a lot of time
+    if (this.length != array.length)
+        return false;
+
+    for (var i = 0, l=this.length; i < l; i++) {
+        // Check if we have nested arrays
+        if (this[i] instanceof Array && array[i] instanceof Array) {
+            // recurse into the nested arrays
+            if (!this[i].compare(array[i]))
+                return false;
+        }
+        else if (this[i] != array[i]) {
+            // Warning - two different object instances will never be equal: {x:20} != {x:20}
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log("first set of tests, should all return true:");
+console.log( [-1].compare( ins.insert([], -1) ) );
+console.log( [-1, 0].compare(ins.insert([0], -1) ) );
+console.log( [0, 1].compare( ins.insert([0], 1) ) );
+console.log( [0, 1, 2, 3].compare( ins.insert( [0, 1, 3], 2) ) );
+console.log( [0, 1, 1.5, 1.5, 2, 3].compare( ins.insert( [0, 1, 1.5, 2, 3], 1.5) ) );
+
+console.log("second set of tests:");
+var sorted_list = [];
+var ans = [-1];
+var new_element = -1;
+var i = ins.getIndexPositionToInsert(sorted_list, new_element);
+var new_list =  ins.insertIntoPosition(sorted_list, new_element, i);
+// console.log("got: ", new_list)
+// console.log("expected", ans)
+console.log( ans.compare(new_list) );
+
+var sorted_list = [0];
+var new_element = -1;
+var ans =[-1, 0];
+var i = ins.getIndexPositionToInsert(sorted_list, new_element);
+var new_list =  ins.insertIntoPosition(sorted_list, new_element, i);
+// console.log("got: ", new_list)
+// console.log("expected", ans)
+console.log( ans.compare(new_list) );
+
+var sorted_list = [0];
+var new_element = 1;
+var ans = [0, 1];
+var i = ins.getIndexPositionToInsert(sorted_list, new_element);
+var new_list =  ins.insertIntoPosition(sorted_list, new_element, i);
+// console.log("got: ", new_list)
+// console.log("expected", ans)
+console.log( ans.compare(new_list) );
+
+var sorted_list = [0, 1, 3];
+var new_element = 2;
+var ans = [0, 1, 2, 3];
+var i = ins.getIndexPositionToInsert(sorted_list, new_element);
+var new_list =  ins.insertIntoPosition(sorted_list, new_element, i);
+// console.log("got: ", new_list)
+// console.log("expected", ans)
+console.log( ans.compare(new_list) );
+
+var sorted_list = [0, 1, 2, 3];
+var new_element = 1.5;
+var ans = [0, 1, 1.5, 2, 3];
+var i = ins.getIndexPositionToInsert(sorted_list, new_element);
+var new_list =  ins.insertIntoPosition(sorted_list, new_element, i);
+// console.log("got: ", new_list)
+// console.log("expected", ans)
+console.log( ans.compare(new_list) );
