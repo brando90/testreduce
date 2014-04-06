56168366fce06fef846deec849e41ea6e54e227e
Brando Miranda <brandomiranda@ubuntu.(none)>
2014-04-06 15:14:38 -0400

    debuged the new API for the insert function, should be working now.

--- insert_function.js
+++ insert_function.js
@@ -3,7 +3,6 @@ function insert(array, element){
     return [element];
   }
   var index = getIndexPositionToInsert(array, element);
-  array.splice(index, 0, element)
   return insertIntoPosition(array, element, index);
 }
 
@@ -13,7 +12,7 @@ function insertIntoPosition(array, element, index){
 }
 
 function getIndexPositionToInsert(array, element){
-  return positionToInsertRec(array, element, 0, array.length);
+  return positionToInsertRec(array, element, 0, array.length - 1);
 }
 
 function positionToInsertRec(array, element, i, j){
@@ -27,18 +26,18 @@ function positionToInsertRec(array, element, i, j){
   var index = Math.floor((j - i + 1)/2);
   var middle = array[index];
   if( element <= middle ){
-    return insertRec(array, element, i, index - 1);
+    return positionToInsertRec(array, element, i, index - 1);
   }else{
-    return insertRec(array, element, index + 1, j);
+    return positionToInsertRec(array, element, index + 1, j);
   }
 }
 
 
-// console.log( insert([], -1), " answer = ", [-1] );
-// console.log( insert([0], -1), " answer = ", [-1, 0] );
-// console.log( insert([0], 1), " answer = ", [0, 1] );
-// console.log( insert([0, 1, 3], 2), "answer = ", [0, 1, 2, 3]);
-// console.log( insert([0, 1, 1.5, 2, 3], 1.5), "answer = ", [0, 1, 1.5, 2, 3]);
+console.log( insert([], -1), " answer = ", [-1] );
+console.log( insert([0], -1), " answer = ", [-1, 0] );
+console.log( insert([0], 1), " answer = ", [0, 1] );
+console.log( insert([0, 1, 3], 2), "answer = ", [0, 1, 2, 3]);
+console.log( insert([0, 1, 1.5, 2, 3], 1.5), "answer = ", [0, 1, 1.5, 1.5, 2, 3]);
 
 exports.insert = insert;
 exports.insertIntoPosition = insertIntoPosition;
